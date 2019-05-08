adc47865a301c8b69299a135e4f6cb5627baa565
Ramya Gangula <ramya1331@gmail.com>
2019-05-07 22:17:23 -0500

    added code to connect with mysql db

--- application.py
+++ application.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mssql+pymssql://username@sqlpythondb:Password1@sqlpythondb.database.windows.net:1433/SQLPythonDB?charset=UTF-8"
+db = SQLAlchemy(app)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
 
-if __name__ == "__main__":
-    app.run()    
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+
+
+db.session.add(Person(id=5,username="Flask", email="example@example.com"))
+db.session.commit()
+
+users = Person.query.all()
